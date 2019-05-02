d37dfef90155a2f1a9a668d1d6c48414e4e4eb5f
alfonsocv12 <alfonsocvu@gmail.com>
2019-05-02 12:10:25 -0600

    Se repararon credenciales

--- routes/routes.py
+++ routes/routes.py
@@ -8,13 +8,16 @@ from controllers.user_controller import UserController
 from controllers.lista_controller import ListaController
 try:
     from env import os
+    cred = credentials.Certificate(os.environ.get('firebase_json'))
+    firebase_admin.initialize_app(cred)
 except:
-    pass
-    
+    firestore_cred = os.environ.get('firebase_json')
+    firestore_cred = json.loads(json)
+    cred = credentials.Certificate(firestore_cred)
+    firebase_admin.initialize_app(cred)
+
 application = bottle.default_app()
 application.error_handle = handler
-cred = credentials.Certificate(os.environ.get('firebase_json'))
-firebase_admin.initialize_app(cred)
 db = firestore.client()
 response.content_type = 'application/json'
 
